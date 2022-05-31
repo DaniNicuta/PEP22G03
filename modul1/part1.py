b1306f68bbba122588758c1fe71adf6781b42b97
DaniNicuta <dani_nicuta@yahoo.com>
2022-05-31 21:25:55 +0300

    my first file

---
+++ modul1/part1.py
@@ -0,0 +1,25 @@
+# print('Hello World', 'My Object', end='\n', sep=' - abcd - ')
+#
+# print('print')
+#
+# print('###')
+# print('# #')
+# print('###')
+#
+# print('3')
+# print(3)
+
+print(3 + 3)
+print('3' + '3')
+print('3' * 5)
+print(3 * 3)
+
+print(30 * '#')
+print("#", " " * 28, "#", sep="")
+print("#" * 30)
+
+my_value = "My Object"
+# my_value = 15
+my_value2 = 10
+
+print(my_value * my_value2)
