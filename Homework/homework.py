f34828f62672353d721cdc3470139e506b65b4ac
DaniNicuta <dani_nicuta@yahoo.com>
2022-06-04 23:28:55 +0300

    my first file

--- Homework/homework.py
+++ Homework/homework.py
@@ -6,6 +6,16 @@
 #  print(result)
 
 #  Quest 2
+#  name1 = input('User 1: ')
+#  name2 = input('User 2: ')
+#  print('Numarul de caractere user 1:', len(name1))
+#  print('Numarul de caractere este egal?', len(name1) == len(name2))
+#  print('Numarul de caractere al primului nume este mai mare?', len(name1) > len(name2))
+#  print('Initiala primului nume este: ', name1[0])
+#  print('Primul nume inversat: ', name1[::-1])
+
+#  Quest 3
+
 name1 = input('User 1: ')
 name2 = input('User 2: ')
 print('Numarul de caractere user 1:', len(name1))
@@ -13,3 +23,9 @@ print('Numarul de caractere este egal?', len(name1) == len(name2))
 print('Numarul de caractere al primului nume este mai mare?', len(name1) > len(name2))
 print('Initiala primului nume este: ', name1[0])
 print('Primul nume inversat: ', name1[::-1])
+number = input('Introduceti un numar:')
+number = int(number)
+print(name1 * number)
+
+#  Quest 4
+fruit = "Ananas"
