9d272ef75f30da8e65346c5f0604ba84e89d3e77
DaniNicuta <dani_nicuta@yahoo.com>
2022-07-10 15:49:11 +0300

    test

--- Dani/Dani.py
+++ Dani/Dani.py
@@ -328,3 +328,20 @@
 # print(my_list.pop())
 # print(my_list.remove(2))
 # print(my_list)
+import pygame
+
+run = True
+width = 400
+height = 100
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont(None, 48)
+text = font.render("The begining", True, (255, 255, 255))
+screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+pygame.display.flip()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT\
+        or event.type == pygame.MOUSEBUTTONUP\
+        or event.type == pygame.KEYUP:
+            run = False
