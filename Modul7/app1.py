a35bdb13d85b5f9d9b14941979d0230ca2626237
DaniNicuta <dani_nicuta@yahoo.com>
2022-07-19 22:11:23 +0300

    my first file

--- Modul7/app1.py
+++ Modul7/app1.py
@@ -1,13 +1,13 @@
 class Product:
 
     def __init__(self):
-        self.cathegory = input(' INtroduceti numele categoriei ')
-        self.name = input(' Introduceti numele produsului ')
+        self.cathegory = input(' Introduceti numele categoriei ')
+        self.name = input(' Introduceti tipul produsului ')
         self.price = input(' Introduceti pretul  ')
         self.stock = input(' Introduceti stocul  ')
 
     def __repr__(self):
-        return 10 * ' = ' + '\n' + 7*' '+ f'Categoria : {self.cathegory}' + '\n' + 10 * ' = ' + '\n' + 10 * ' = ' + '\n' + 7*' '+ f'Nume produs : {self.name}' + '\n' + 10 * ' = ' + '\n' + 7*' '+ f'Pret : {self.price}' + '\n'  + 10 * ' = ' + '\n' + 7*' '+ f'Stoc : {self.stock}' + '\n' + 10 * ' = '
+        return 10 * ' = ' + '\n' + 7*' '+ f'Categoria : {self.cathegory}' + '\n'  + 10 * ' = ' + '\n' + 7*' '+ f'Nume produs : {self.name}' + '\n' + 10 * ' = ' + '\n' + 7*' '+ f'Pret : {self.price}' + '\n'  + 10 * ' = ' + '\n' + 7*' '+ f'Stoc : {self.stock}' + '\n' + 10 * ' = '
 
 
 if __name__ == '__main__':
